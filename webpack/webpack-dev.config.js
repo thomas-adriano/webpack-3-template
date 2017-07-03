55b2ed2727680a91901f447ae98b927f9cac2b3e
Thomas Oelke Adriano <thomas@Thomass-MBP.home>
2017-07-03 17:17:15 -0300

    -css and js caching completelly implemented; -added old browsers fallbacks (html5shiv, respondjs, modernizr classes);

--- webpack/webpack-dev.config.js
+++ webpack/webpack-dev.config.js
@@ -5,9 +5,24 @@ const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 
 configs.devtool = 'inline-source-map';
 configs.output.filename = '[name].js';
+configs.module.rules = configs.module.rules.concat([{
+    test: /\.css$/,
+    use: [
+        'style-loader',
+        {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+        },
+        'postcss-loader'
+    ]
+}, ]);
+
 configs.plugins = configs.plugins.concat([
     //creates the html file even in webpack-dev-server sessions
     new HtmlWebpackHarddiskPlugin(),
+
+    //replaces module IDs with paths to the modules 
+    //keeps module IDs stable as they aren't derived based on order. 
     new webpack.NamedModulesPlugin(),
 ]);
 
--- webpack/webpack-prod.config.js
+++ webpack/webpack-prod.config.js
@@ -1,5 +1,8 @@
 const configs = require('./webpack-commons.config.js');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 configs.devtool = 'cheap-module-source-map';
 // https://webpack.js.org/configuration/output/#output-publicpath
@@ -10,50 +13,74 @@ configs.devtool = 'cheap-module-source-map';
 // The value of the option is prefixed to every URL created 
 // by the runtime or loaders. Because of this the value of 
 // this option ends with /
-configs.output.publicPath = 'http://localhost:8081/';
+// configs.output.publicPath = 'http://localhost:8081/';
 configs.output.filename = '[name].[chunkhash].js';
 
 configs.module.rules = configs.module.rules.concat([{
-    test: /\.(gif|png|jpe?g|svg)$/,
-    use: [{
-        loader: 'image-webpack-loader',
-        query: {
-            progressive: true,
-            pngquant: {
-                optimizationLevel: 7,
-                interlaced: true,
-                quality: '65-90',
-                speed: 4
-            },
-            mozjpeg: {
-                quality: 65
-            },
-            gifsicle: {
-                optimizationLevel: 7,
-                interlaced: true,
-                optimizationLevel: 2
+        test: /\.(gif|png|jpe?g|svg)$/,
+        use: [{
+            loader: 'image-webpack-loader',
+            query: {
+                progressive: true,
+                pngquant: {
+                    optimizationLevel: 7,
+                    interlaced: true,
+                    quality: '65-90',
+                    speed: 4
+                },
+                mozjpeg: {
+                    quality: 65
+                },
+                gifsicle: {
+                    optimizationLevel: 7,
+                    interlaced: true,
+                    optimizationLevel: 2
+                }
             }
-        }
-    }]
-}, ]);
+        }]
+    },
+    {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'postcss-loader']
+        }),
+    }
+]);
 
 configs.plugins = configs.plugins.concat([
-    //generate identifiers that are preserved over builds (prod version)
+    //generate identifiers that are preserved over builds
     new webpack.DefinePlugin({
         'process.env': {
             'NODE_ENV': JSON.stringify('production')
         }
     }),
+
+    //replaces module IDs with hashes of the modules
+    //keeps module IDs stable as they aren't derived based on order. 
     new webpack.HashedModuleIdsPlugin(),
+
+    new ScriptExtHtmlWebpackPlugin({
+        preload: /\.js$/,
+        sync: /vendor\./,
+        defaultAttribute: 'async'
+    }),
+
+    //extracts css content to separate files
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    new CopyWebpackPlugin([
+        { from: 'node_modules/respond.js/dest/respond.min.js', to: './' },
+        { from: 'node_modules/html5shiv/dist/html5shiv.min.js', to: './' },
+    ]),
+
+    //minifies JS code
     new webpack.optimize.UglifyJsPlugin({
         compress: {
             warnings: true,
         },
         sourceMap: true,
-    }),
-    new webpack.LoaderOptionsPlugin({
-        minimize: true,
-    }),
+    })
 ]);
 
 module.exports = configs;
