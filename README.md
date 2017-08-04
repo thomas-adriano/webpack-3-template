b561ee002dd7d0ca9f3a5b6905a9686c88b7a4d2
Thomas Adriano <thomas.o.adriano@gmail.com>
2017-08-03 22:40:26 -0300

    Readme changed, project.config.js documented

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# webpack2-es6-cssnext
-webpack2-es6-cssnext
+# Cache ready
+* All assets used (like images, js and css files) are minified, compressed and have the name changed to its content hash;
+* Thirdparty code (provenient from sources other than you) separated from application code;
+* Native app like experience (work offline, has desktop icon, is full screen) with Service Workers and manifest.json.
--- project.configs.js
+++ project.configs.js
@@ -1,15 +1,22 @@
+/**
+ * This script gathers all project related configurations. 
+ * Besides properties, it also homes some utility function such as getRootRelativePath.
+ * 
+ * All paths must be absolute, the appendRootPath can be used to facilitate this.
+ */
+
 const path = require("path");
 
-const ROOT_PATH = path.resolve('.');
-const BUILD_PATH = path.join(ROOT_PATH, 'build');
-const INDEX_JS_PATH = path.join(ROOT_PATH, 'src/index/index.js');
-const INDEX_HTML_PATH = path.join(ROOT_PATH, 'src/index/index.html');
-const FAVICON_PATH = path.join(ROOT_PATH, 'assets/favicon/js.png');
+const ROOT_PATH = __dirname;
+const BUILD_PATH = appendRootPath("build");
+const INDEX_JS_PATH = appendRootPath("src/index/index.js");
+const INDEX_HTML_PATH = appendRootPath("src/index/index.html");
+const FAVICON_PATH = appendRootPath("assets/favicon/js.png");
 const INLINE_ASSETS_MAX_SIZE_IN_BYTES = 10000;
 const APP_TITLE = "Webpack 3 Template";
 const PUBLIC_PATH = "";
 const DEV_SERVER_PORT = 8081;
-const DEV_SERVER_HOST = "0.0.0.0"
+const DEV_SERVER_HOST = "0.0.0.0";
 
 module.exports = {
     getRootRelativePath,
@@ -22,17 +29,28 @@ module.exports = {
     FAVICON_PATH,
     DEV_SERVER_PORT,
     DEV_SERVER_HOST
+};
+
+/**
+  * Given an absolute filesystem path, returns the path relative to the root path.
+  * The root path can be defines with the property ROOT_PATH.
+  * 
+  * @param {string} strPath absolute filesystem path
+  * @returns path relative to the root path
+  */
+function getRootRelativePath(strPath) {
+    return strPath.replace(normalizePath(ROOT_PATH), "");
+}
+
+function appendRootPath(childPath) {
+    return path.join(ROOT_PATH, childPath);
 }
 
 function normalizePath(strPath) {
     const lastPathSeparatorIndex = strPath.lastIndexOf(path.sep);
-    const hasTrailingSlash = lastPathSeparatorIndex === (strPath.length - 1)
+    const hasTrailingSlash = lastPathSeparatorIndex === strPath.length - 1;
     if (hasTrailingSlash) {
         return strPath;
     }
     return strPath + path.sep;
-}
-
-function getRootRelativePath(strPath) {
-    return strPath.replace(normalizePath(ROOT_PATH), "");
 }
